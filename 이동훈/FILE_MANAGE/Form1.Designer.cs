7b7e67bfe7d52fa25f6e8ce9b4d4627f5d66c78e
wkkjhhhhh <qw8421@naver.com>
2024-07-08 13:27:53 +0900

    format

---
+++ 이동훈/FILE_MANAGE/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace image_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.download = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(640, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(321, 41);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(313, 25);
+            this.download.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(177, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Download";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(177, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File Upload";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(640, 107);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(181, 162);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(561, 259);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox download;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 이동훈/FILE_MANAGE/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Azure 스토리지에서 다운로드하고자 하는 파일명
+            string blobName = download.Text;
+            // Azure 스토리지로부터 다운로드할 파일을 저장할 경로 + 파일명
+            // ex) D://test/images/dog.jpg
+            string downloadFilePath = "C://Users/ldh97/Desktop/images/"+download.Text;
+
+            await DownloadFileAsync(blobName, downloadFilePath);
+        }
+
+        // Azure 스토리지 연결 문자열
+        // 보안 문제 상 연결 문자열은 비공개, KAKAO TALK 참고
+        //private string connectionString = "Azure Storage Connect text";
+        private string connectionString = "DefaultEndpointsProtocol=https;AccountName=kioskproject;AccountKey=7WdD4X1zax7SKV5ouf+svEtku2v7buHUWt/BeNNsY0mS4kETH/UyZP4WpaNbi7LpxPRoobEUezWY+AStK0dOGA==;EndpointSuffix=core.windows.net";
+        // Azure 스토리지 명
+        private string containerName = "kiosk";
+
+        public async Task DownloadFileAsync(string blobName, string downloadFilePath)
+        {
+            // Azure 스토리지 클라이언트 연결
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+
+            // 다운로드 할 Blob에 대한 참조 가져오기
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            bool exists = await blobClient.ExistsAsync();
+
+            if(!exists)
+            {
+                // 다운로드 하려는 파일이 Azure 스토리지에 없는 경우
+                MessageBox.Show("Azure Storage에 해당 파일이 존재하지 않습니다.", "DOWNLOAD ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("다운로드를 시작합니다.", "DOWNLOAD", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Blob의 내용을 다운로드하여 BlobDownloadInfo 객체에 저장
+                BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+
+                // 로컬 파일 시스팀에 다운로드한 Blob의 내용을 저장
+                using (FileStream fs = File.OpenWrite(downloadFilePath))
+                {
+                    /*
+                     C# using 문
+
+                    1. 특정 객체를 사용한 후 자동으로 리소스를 해제하도록 사용 됨
+                    2. 주로 파일, 데이터베이스 연결, 네트워크 연결 등과 같이 직접적으로 시스템 리소스를 사용하는 객체와 함께 사용됨
+
+                    hoon
+                    {
+                        using문을 잘 활용하면 예외처리할 때 도움이 될 듯 함.
+                    }
+                     */
+
+                    await download.Content.CopyToAsync(fs);
+                    fs.Close();
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
